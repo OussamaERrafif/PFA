ee0d0669fb5956e2ffac4560f97a622108722e0f
OussamaERrafif <oussamaerra2002@gmail.com>
2024-03-29 02:15:53 +0000

    3/29/2024 2:15AM

--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { AccountComponent } from './Profile/Account';
 import { SecurityComponent } from './Profile/Security';
+import { PlanAndBillingsComponent } from './Profile/planandbilings';
 
 @Component({
     selector: 'app-home',
@@ -9,9 +10,10 @@ import { SecurityComponent } from './Profile/Security';
     template: `
     <app-account></app-account>
     <app-security></app-security>
+    <app-plan-and-billings></app-plan-and-billings>
 
   `,
-    imports: [NgIf , AccountComponent, SecurityComponent]})
+    imports: [NgIf , AccountComponent, SecurityComponent,PlanAndBillingsComponent]})
 export default class HomeComponent {
   
 }
